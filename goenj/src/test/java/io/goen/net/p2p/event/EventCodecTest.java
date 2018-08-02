85ea39984159b06ab35e5006d9ada55b90648b19
HennChou <hennchou@gmail.com>
2018-08-03 01:45:07 +0800

    upload p2p test

--- goenj/src/test/java/io/goen/net/p2p/event/EventCodecTest.java
+++ goenj/src/test/java/io/goen/net/p2p/event/EventCodecTest.java
@@ -6,6 +6,7 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.channel.socket.DatagramPacket;
 import org.junit.Test;
+import org.spongycastle.util.encoders.Hex;
 
 import java.net.InetSocketAddress;
 
@@ -14,6 +15,8 @@ import static org.junit.Assert.*;
 public class EventCodecTest {
     @Test
     public void encode() throws Exception {
+
+
         PingEvent pingEvent = new PingEvent();
         pingEvent.setExpires(1533218340901L+100);
         pingEvent.setFromIp("192.168.1.1");
@@ -25,7 +28,7 @@ public class EventCodecTest {
         P2PMessage p2pMessage = new P2PMessage(InetSocketAddress.createUnresolved(pingEvent.getToIp(),pingEvent.getToPort()),pingEvent);
 
         EmbeddedChannel channel = new EmbeddedChannel(new EventCodec());
-
+        //output p2pMessge
         assertTrue(channel.writeOutbound(p2pMessage));
         assertTrue(channel.finish());
 
@@ -34,16 +37,30 @@ public class EventCodecTest {
         ByteBuf buf = packet.content();
         byte[] testEncodeData = new byte[buf.readableBytes()];
         buf.readBytes(testEncodeData);
+
         PingEvent decodePingEvent = new PingEvent();
         decodePingEvent.parse(testEncodeData);
+
         assertEquals(decodePingEvent.getToIp(),pingEvent.getToIp());
+        assertEquals(decodePingEvent.getToPort(),pingEvent.getToPort());
+        assertEquals(decodePingEvent.getFromIp(),pingEvent.getFromIp());
+        assertEquals(decodePingEvent.getFromPort(),pingEvent.getFromPort());
+        assertEquals(decodePingEvent.getRandomHexString(),pingEvent.getRandomHexString());
 
     }
 
     @Test
     public void decode() throws Exception {
         ByteBuf buf = Unpooled.buffer();
+        String hexString = "f66a5e9adc47a8934767f1ca6f3da51f5511385b1877159f8e71b5052d7686100101525ec363cd90a84b26ca47bcb04dcb95c90bb88589d6ee9093bd909e9727b9d067ad466132032a5779794e78e9346be5205fca2f8b12ac0a4bd31cd1f57b8da900dd84c0a8010282762584c0a8010182762583abcdef8800000164faef4089";
 
+        buf.writeBytes(Hex.decode(hexString));
+        ByteBuf input = buf.duplicate();
+        DatagramPacket packet = new DatagramPacket(input,InetSocketAddress.createUnresolved("192.168.1.1",30245));
+        EmbeddedChannel channel = new EmbeddedChannel(new EventCodec());
+        channel.writeInbound(packet);
+        channel.finish();
+        P2PMessage p2PMessage = channel.readInbound();
     }
 
     public static void main(String[] args) {
