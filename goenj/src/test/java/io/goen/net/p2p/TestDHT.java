1d45288cc7df8d3a3f98162ab1abe2ff44912502
hennchou <hennchou@gmail.com>
2018-08-28 02:07:53 +0800

    update test

--- goenj/src/test/java/io/goen/net/p2p/TestDHT.java
+++ goenj/src/test/java/io/goen/net/p2p/TestDHT.java
@@ -14,8 +14,16 @@ public class TestDHT {
     public void testDHT(){
         byte[] selfId = HashUtil.sha256(GoenConfig.system.publicKey());
         Node node = new  Node(selfId, InetAddresses.forString("127.0.0.1"),30222);
+        Node nodeA = new  Node(HashUtil.sha256("nodeA".getBytes()), InetAddresses.forString("127.0.0.5"),30222);
+        Node nodeB = new  Node(HashUtil.sha256("nodeB".getBytes()), InetAddresses.forString("127.0.0.2"),30222);
+        Node nodeC = new  Node(HashUtil.sha256("nodeC".getBytes()), InetAddresses.forString("127.0.0.3"),30222);
+        Node nodeD = new  Node(HashUtil.sha256("nodeD".getBytes()), InetAddresses.forString("127.0.0.4"),30222);
         DistributedHashTable distributedHashTable =  new DistributedHashTable(node);
-        distributedHashTable.instertNode(node);
+        distributedHashTable.instertNode(nodeA);
+        distributedHashTable.instertNode(nodeB);
+        distributedHashTable.instertNode(nodeC);
+        distributedHashTable.instertNode(nodeD);
+
     }
 
 }
