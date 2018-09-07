35f1a58d8e85c3338f51c0cf971740b97f25acd0
hennchou <hennchou@gmail.com>
2018-09-07 21:53:59 +0800

    update net

---
+++ goenj/src/main/java/io/goen/net/goen/GoenEngine.java
@@ -0,0 +1,15 @@
+package io.goen.net.goen;
+
+import io.goen.net.Engine;
+
+public class GoenEngine implements Engine {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
